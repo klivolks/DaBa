2bc4a882d9198c123b12131a6cf2f6045ac29f5d
klivolks <klivolks@gmail.com>
2023-05-28 20:23:04 +0100

    v0.0.4 - closing connection before execution issue solved

--- daba/Mongo.py
+++ daba/Mongo.py
@@ -91,11 +91,13 @@ class collection:
         return self.exec_operation(self.collection.count_documents, condition)
 
     def exec_operation(self, operation, *args):
+        response = None
         try:
             response = operation(*args)
-            return response
         except Exception as e:
             self.logger.error(f"An error occurred: {e}")
             raise
         finally:
-            self.close_db()
+            if not response:  # only close the db connection if operation failed
+                self.close_db()
+        return response
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name='daba',
-    version='0.0.3',
+    version='0.0.4',
     description='daba by Klivolks.',
     long_description=long_description,
     long_description_content_type='text/markdown',
