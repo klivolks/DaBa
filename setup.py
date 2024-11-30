40fc59c0d408c30cdf7c9c981b5291d864a6db7f
klivolks <klivolks@gmail.com>
2024-11-30 23:50:08 +0000

    v1.0.1
    - first production release
    - implemented create index
    - publish error fix

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     url='https://github.com/klivolks/DaBa',
     packages=find_packages(),
     classifiers=[
-        'Development Status :: 5 - Production/Stables',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python :: 3.9',
